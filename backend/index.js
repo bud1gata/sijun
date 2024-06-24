89eac99d9ce76d0b8910c9ce8eec2fc8b7472a67
bud1gata <bud1gata@gmail.com>
2024-06-24 11:54:21 +0700

    init: update index.js, package.json

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { PORT } from "./config/config";
+import { PORT } from "./config/config.js";
 
 const app = express();
+
 app.listen(PORT, () => {
     console.log(`Aplikasi berjalan pada Port: ${PORT}`);
 });
